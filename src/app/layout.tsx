50c40389a0e06841eb3e50f9122ec9dc4d9cd89a
jocelyn <jcln@student.ubc.ca>
2025-01-15 09:51:19 -0800

    New favicon, updated fonts to use my own style, made things more silly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const manrope = Manrope({weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Jocelyn Baker",
@@ -23,12 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+      <html lang="en">
+      <body className={manrope.className}>{children}</body>
+</html>
+)
+    ;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,53 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import {fadeRiseIn} from "@/api/animations";
+import {SetStateAction, useState} from "react";
+
+const titles = ["Great Job!", "Wow, so proud of you", "You're so good at clicking buttons!", "Okay, maybe that's enough",
+    "Ok, you've had your fun", "You can stop clicking now", "PLEASE STOP", "I BEG YOU STOP CLICKING", "Fine.", "Whatever.",
+    "I'm ignoring you", "  ", "  ", "     ", "Now you've done it.", "You don't know what's coming for you",
+    "You should stop for your own good", "I'm warning you", "You've done it now.", ""];
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <motion.div initial={"initial"} animate={"animate"} variants={fadeRiseIn}>
-          <Image
-          className="dark:invert"
-          src="/signature.svg"
-          alt="Jocelyn Baker"
-          width={180}
-          height={180}
-          priority
-          />
-        </motion.div>
-        <p className={"text-md"}>Let&apos;s get 417-ing!</p>
-      </main>
-    </div>
-  );
+    const [title, setTitle] = useState("Click for 417");
+    const [secret, setSecret] = useState(false);
+
+    return (
+        <div
+            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                <div className={"flex flex-col text-center items-center space-y-4"}>
+                    <motion.div initial={"initial"} animate={"animate"} variants={fadeRiseIn}>
+                        <Image
+                            className="dark:invert"
+                            src="/signature.svg"
+                            alt="Jocelyn Baker"
+                            width={180}
+                            height={180}
+                            priority
+                        />
+                    </motion.div>
+                    <h2 className={"p-4 min-w-24 min-h max-w-md text-wrap text-lg rounded-lg border-2 border-neutral-500"}
+                        onClick={() => setTitle(snarkyTitle(setSecret))}>{title}&#8203;</h2>
+                    {secret &&
+                        <h2 className={"p-4 min-w-3 max-w-md text-wrap text-lg rounded-lg border-2 border-black bg-neutral-900 text-white dark:invert"}>
+                            Anticlimax
+                        </h2>
+                    }
+                </div>
+            </main>
+        </div>
+    );
+}
+
+let count = 0;
+
+function snarkyTitle(secretStateHook: { (value: SetStateAction<boolean>): void; (arg0: boolean): void; }): string {
+    if (count < titles.length) {
+        return titles[count++];
+    } else {
+        secretStateHook(true);
+        return titles[titles.length - 1];
+    }
 }
