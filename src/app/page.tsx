a372f4f63002def6fcbaa19f2364ef1246431834
jocelyn <jcln@student.ubc.ca>
2025-01-15 11:22:57 -0800

    Much better silly feature and better button feedback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,19 @@ import {SetStateAction, useState} from "react";
 const titles = ["Great Job!", "Wow, so proud of you", "You're so good at clicking buttons!", "Okay, maybe that's enough",
     "Ok, you've had your fun", "You can stop clicking now", "PLEASE STOP", "I BEG YOU STOP CLICKING", "Fine.", "Whatever.",
     "I'm ignoring you", "  ", "  ", "     ", "Now you've done it.", "You don't know what's coming for you",
-    "You should stop for your own good", "I'm warning you", "You've done it now.", ""];
+    "You should stop for your own good", "I'm warning you", "You've done it now.", "Congratulations!"];
 
 
 export default function Home() {
     const [title, setTitle] = useState("Click for 417");
     const [secret, setSecret] = useState(false);
+    const [start, setStart] = useState(Date.now());
 
     return (
         <div
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <div className={"flex flex-col text-center items-center space-y-4"}>
+            <main className="flex row-start-2 items-center sm:items-start">
+                <div className={"flex flex-col gap-4 text-center items-center"}>
                     <motion.div initial={"initial"} animate={"animate"} variants={fadeRiseIn}>
                         <Image
                             className="dark:invert"
@@ -29,13 +30,21 @@ export default function Home() {
                             priority
                         />
                     </motion.div>
-                    <h2 className={"active:bg-neutral-300 bg-transparent select-none transition-all duration-150 p-4 min-w-24 min-h max-w-md text-wrap text-lg rounded-lg border-2 border-neutral-500"}
-                        onClick={() => setTitle(snarkyTitle(setSecret))}>{title}&#8203;</h2>
-                    {secret &&
-                        <h2 className={"p-4 min-w-3 max-w-md text-wrap text-lg rounded-lg border-2 border-black bg-neutral-900 text-white dark:invert"}>
-                            Anticlimax
-                        </h2>
-                    }
+                    <div className={"space-y-4"}>
+                        <h2 className={"w-fit select-none transition-all duration-200 hover:outline-neutral-300 " +
+                            "hover:outline-4 active:bg-neutral-500 p-4 min-w-24 max-w-md " +
+                            "text-wrap text-lg rounded-lg outline outline-0 outline-transparent" +
+                            " bg-neutral-700 text-white dark:invert"}
+                        onClick={() => setTitle(snarkyTitle(setSecret, setStart))}>{title}&#8203;</h2>
+                        {secret &&
+                            <h2 className={"select-none transition-all duration-200 hover:outline-neutral-300 " +
+                                "hover:outline-4 active:bg-neutral-700 p-4 min-w-3 max-w-md " +
+                                "text-wrap text-lg rounded-lg outline outline-0 outline-transparent" +
+                                " bg-neutral-900 text-white dark:invert"}>
+                                You have just wasted {((Date.now() - start)/1000).toFixed(0)} seconds
+                            </h2>
+                        }
+                    </div>
                 </div>
             </main>
         </div>
@@ -44,8 +53,12 @@ export default function Home() {
 
 let count = 0;
 
-function snarkyTitle(secretStateHook: { (value: SetStateAction<boolean>): void; (arg0: boolean): void; }): string {
-    if (count < titles.length) {
+function snarkyTitle(secretStateHook: { (value: SetStateAction<boolean>): void; (arg0: boolean): void; },
+                     timeHook: (value: (((prevState: number) => number) | number)) => void): string {
+    if (count == 0) {
+        timeHook(Date.now())
+    }
+    if (count < titles.length - 1) {
         return titles[count++];
     } else {
         secretStateHook(true);
