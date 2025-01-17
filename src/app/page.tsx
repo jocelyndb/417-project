47a60cf850cc5edaea99593c88f489a7024b7ecd
Jocelyn Baker <jocelyndbaker@gmail.com>
2025-01-17 03:30:06 +0000

    Disable selection on logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <div
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex row-start-2 items-center sm:items-start">
-                <motion.div initial={"initial"} animate={"animate"} className={"flex flex-col gap-4 text-center items-center"}>
+                <motion.div initial={"initial"} animate={"animate"} className={"flex flex-col gap-4 text-center items-center select-none"}>
                     <motion.div variants={fadeRiseIn}>
                         <Image
                             className="dark:invert"
